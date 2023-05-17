1e9e1133cf6d3400b357eb90bea47b1c47712998
AHardworkingMan <1292162626@qq.com>
2023-05-17 11:20:17 +0800

    update some codes

---
+++ inc/tcp_server.h
@@ -0,0 +1,23 @@
+
+#ifndef TCP_SERVER_H_
+#define TCP_SERVER_H_
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "common.h"
+
+class tcp_server {
+public:
+    tcp_server(void) ;
+    ~tcp_server(void) ;
+    int tcp_server_create(void);
+private:
+    int server_shm_init(void);
+};
+
+//int tcp_server_create(void);
+
+#endif // TCP_SERVER_H_
