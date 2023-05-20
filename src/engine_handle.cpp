4e1804ad1fb4a0ee152c158bfb40e2692f6c55f9
Knys-x <2471127906@qq.com>
2023-05-20 14:43:40 +0800

    Update engine_handle.cpp

--- src/engine_handle.cpp
+++ src/engine_handle.cpp
@@ -34,17 +34,17 @@ namespace {
     char mode_remote[20] = "Remote";
     char getting_ip[20] = "getting ip";
 
-    int uart_print_count = 0;
+    int uart_print_count = 0;  // 0: print 1: not print
     oled oled_ctrl;
 
-    struct shared_common_st* shared_stuff;
+    struct shared_common_st* shared_stuff;  // shared memory
 
-    struct PostData post_data;
+    struct PostData post_data;  // post data
 
     void* shared_memory = (void*)0;
-    int shmid;
+    int shmid;  // shared memory id
 
-    int current_work_mode = ASCBOT_ROAD_MODE;//ASCBOT_ROAD_MODE  ASCBOT_OFF
+    int current_work_mode = ASCBOT_ROAD_MODE; // ASCBOT_ROAD_MODE ASCBOT_OFF ASCBOT_REMOTE_MODE ASCBOT_FOLLOW_MODE ASCBOT_DANGE_MODE ASCBOT_IP_MODE ASCBOT_GET_IP_MODE
 
     int current_remote_left_speed = 0;
     int current_remote_right_speed = 0;
@@ -60,33 +60,33 @@ EngineHandle::EngineHandle() {
 
 }
 
-int EngineHandle::common_shm_init(void)
+int EngineHandle::common_shm_init(void)  // 初始化共享内存
 {
     INFO_LOG("common_shm_init_handle------------ \n");
-    shmid = shmget((key_t)1234, sizeof(struct shared_common_st), 0666 | IPC_CREAT);
+    shmid = shmget((key_t)1234, sizeof(struct shared_common_st), 0666 | IPC_CREAT);  // 创造共享内存
 
-    if (shmid == -1) {
-        ERROR_LOG("shmget failed\n");
+    if (shmid == -1) {  // 检查共享内存是否创建成功
+        ERROR_LOG("shmget failed\n");  // 打印错误信息
         return -1;
     }
-     shared_memory = shmat(shmid, (void *)0, 0);
-     if (shared_memory == (void *)-1) {
-         ERROR_LOG("shmat failed\n");
+     shared_memory = shmat(shmid, (void *)0, 0);  // 附加共享内存
+     if (shared_memory == (void *)-1) {  // 检查共享内存是否附加成功
+         ERROR_LOG("shmat failed\n");  // 打印错误信息
         return -1;
     }
-    shared_stuff = (struct shared_common_st *)shared_memory;
-    shared_stuff->written_flag = 0;
+    shared_stuff = (struct shared_common_st *)shared_memory;  // 将共享内存转换为结构体
+    shared_stuff->written_flag = 0;  // 初始化共享内存
     return 0;
 }
 
 
 
-int EngineHandle::common_shm_process_handle(void)
+int EngineHandle::common_shm_process_handle(void)  // 处理共享内存
 {
-    INFO_LOG(" shared_stuff->written_flag:%d shared_stuff->wheel_speed_written_flag:%d\n", shared_stuff->written_flag,shared_stuff->wheel_speed_written_flag);
-    if (shared_stuff->written_flag == 1)
+    INFO_LOG(" shared_stuff->written_flag:%d shared_stuff->wheel_speed_written_flag:%d\n", shared_stuff->written_flag,shared_stuff->wheel_speed_written_flag);  // 打印共享内存写入标志位
+    if (shared_stuff->written_flag == 1)  // 检查共享内存写入标志位
     {
-        post_data.work_mode = shared_stuff->work_mode;  //get work_mode from APK by tcp
+        post_data.work_mode = shared_stuff->work_mode;  // 读取共享内存
         post_data.remote_direction = shared_stuff->remote_direction;
         post_data.remote_left_speed = shared_stuff->remote_left_speed;
         post_data.remote_right_speed = shared_stuff->remote_right_speed;
@@ -99,43 +99,43 @@ int EngineHandle::common_shm_process_handle(void)
         post_data.wheel_right_speed =  shared_stuff->wheel_right_speed;
         shared_stuff->wheel_speed_written_flag = 0;
 
-        if(shared_stuff->wheel_force_stop == 1)  //check road mode
+        if(shared_stuff->wheel_force_stop == 1)  // 强制停止
         {
-            post_data.wheel_left_speed = 0;
-            post_data.wheel_right_speed = 0;
+            post_data.wheel_left_speed = 0;  // 左轮速度
+            post_data.wheel_right_speed = 0;  // 右轮速度
         }
     }
 
-    if(shared_stuff->ip_written_flag == 1)
+    if(shared_stuff->ip_written_flag == 1)  // 获取ip
     {
-        strncpy(post_data.ip_address , shared_stuff->ip_address,20);
-        shared_stuff->ip_written_flag = 0;
-        post_data.ip_get_flag = 1;
-        ERROR_LOG("post_data.ip_address------------- %s  \n",post_data.ip_address);
+        strncpy(post_data.ip_address , shared_stuff->ip_address,20);  // ip地址
+        shared_stuff->ip_written_flag = 0;  // 重置标志位
+        post_data.ip_get_flag = 1;  // ip获取标志位
+        ERROR_LOG("post_data.ip_address------------- %s  \n",post_data.ip_address);  // 打印ip地址
     }
-    INFO_LOG("left_speed:%d,right_speed:%d\n",post_data.wheel_left_speed,post_data.wheel_right_speed);
+    INFO_LOG("left_speed:%d,right_speed:%d\n",post_data.wheel_left_speed,post_data.wheel_right_speed);  // 打印左右轮速度
     return 0;
 }
 
 int EngineHandle::Init( ) {
     INFO_LOG("Begin EngineHandle initialize!");
-    tcp_server tcp_server_ctrl;
-    tcp_server_ctrl.tcp_server_create();
-    wheel_ctrl.wheel_init();
+    tcp_server tcp_server_ctrl;  // tcp服务器
+    tcp_server_ctrl.tcp_server_create();  // 创建tcp服务器
+    wheel_ctrl.wheel_init();  // 初始化电机车轮控制
 
     common_shm_init();
 
     oled_ctrl.OLED_Init();
     oled_ctrl.OLED_CLS_RAM();
 
-    oled_ctrl.OLED_ShowStr(0, 0, getting_ip, 1);
-    oled_ctrl.OLED_ShowStr(1, 0, mode_off, 1);
-    oled_ctrl.OLED_Screen_display();
+    oled_ctrl.OLED_ShowStr(0, 0, getting_ip, 1);  // 显示ip获取
+    oled_ctrl.OLED_ShowStr(1, 0, mode_off, 1);  // 显示模式
+    oled_ctrl.OLED_Screen_display();  // 屏幕显示
 
-    post_data.angle = 0;
-    post_data.direction = 0;
-    post_data.work_mode = 0;
-    post_data.ip_get_flag = 0;
+    post_data.angle = 0;  // 角度
+    post_data.direction = 0;  // 方向
+    post_data.work_mode = 0;  // 工作模式
+    post_data.ip_get_flag = 0;  // ip获取标志位
 
     INFO_LOG("End EngineHandle initialize!");
     return 0;
@@ -144,29 +144,29 @@ int EngineHandle::Init( ) {
 int EngineHandle::hand_all(void)
 {
     common_shm_process_handle();
-    if(post_data.ip_get_flag == 1) //OLED
+    if(post_data.ip_get_flag == 1)  // 对比ip标志位
     {
-        oled_ctrl.OLED_RowClear(0,1);
-        oled_ctrl.OLED_ShowStr(0, 0, post_data.ip_address, 1);
-        oled_ctrl.OLED_Screen_display();
-        post_data.ip_get_flag = 0;
+        oled_ctrl.OLED_RowClear(0,1);  // 清除第一行
+        oled_ctrl.OLED_ShowStr(0, 0, post_data.ip_address, 1);  // 显示ip地址
+        oled_ctrl.OLED_Screen_display();  // 屏幕显示
+        post_data.ip_get_flag = 0; // 重置ip标志位
     }
 
-    if(current_work_mode !=post_data.work_mode)
+    if(current_work_mode !=post_data.work_mode)  // 对比工作模式
     {
-        current_work_mode = post_data.work_mode;
+        current_work_mode = post_data.work_mode;  // 更新工作模式
 
-        oled_ctrl.OLED_RowClear(1,1);
-        if(current_work_mode==ASCBOT_OFF)
-            oled_ctrl.OLED_ShowStr(1, 0, mode_off, 1);
-        else if(current_work_mode == ASCBOT_VOID_DANGER_MODE)
-            oled_ctrl.OLED_ShowStr(1, 0, mode_dange, 1);
-        else if(current_work_mode == ASCBOT_OBJECT_MODE)
-            oled_ctrl.OLED_ShowStr(1, 0, mode_follow, 1);
-        else if(current_work_mode == ASCBOT_ROAD_MODE)
-            oled_ctrl.OLED_ShowStr(1, 0, mode_road, 1);
-        else if(current_work_mode == ASCBOT_REMOTE_MODE)
-           oled_ctrl.OLED_ShowStr(1, 0, mode_remote, 1);
+        oled_ctrl.OLED_RowClear(1,1);  // 清除第二行
+        if(current_work_mode==ASCBOT_OFF)  // 当前工作模式为关闭模式
+            oled_ctrl.OLED_ShowStr(1, 0, mode_off, 1);  // 显示关闭模式
+        else if(current_work_mode == ASCBOT_VOID_DANGER_MODE)  // 当前工作模式为避障模式
+            oled_ctrl.OLED_ShowStr(1, 0, mode_dange, 1);  // 显示避障模式
+        else if(current_work_mode == ASCBOT_OBJECT_MODE)  // 当前工作模式为物体模式
+            oled_ctrl.OLED_ShowStr(1, 0, mode_follow, 1);  // 显示物体模式
+        else if(current_work_mode == ASCBOT_ROAD_MODE)  // 当前工作模式为道路模式
+            oled_ctrl.OLED_ShowStr(1, 0, mode_road, 1);  // 显示道路模式
+        else if(current_work_mode == ASCBOT_REMOTE_MODE)  // 当前工作模式为遥控模式
+			oled_ctrl.OLED_ShowStr(1, 0, mode_remote, 1);  // 显示遥控模式
 
         oled_ctrl.OLED_Screen_display();
 
@@ -178,7 +178,7 @@ int EngineHandle::hand_all(void)
         shared_stuff->wheel_force_stop = 0;
     }
 
-    INFO_LOG("current:%d,%d\n",current_wheel_left_speed,current_wheel_right_speed);
+    INFO_LOG("current:%d,%d\n",current_wheel_left_speed,current_wheel_right_speed);  // 打印当前左右轮速度
     if((current_wheel_left_speed!=post_data.wheel_left_speed)||(current_wheel_right_speed!=post_data.wheel_right_speed))
     {
         current_wheel_left_speed = post_data.wheel_left_speed;
